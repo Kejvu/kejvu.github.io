21398b3f56507b689976906382571d0c0d2c9480
Vladimir Jovanović <vladimir.jovanovic993@gmail.com>
2019-08-25 21:34:23 +0200

    Email handler

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 (function ($) {
     "use strict";
 
@@ -6,13 +5,27 @@
     [ Validate ]*/
     var input = $('.validate-input .input100');
 
-    $('.validate-form').on('submit',function(){
+    $('.validate-form').on('submit', function (e) {
+        e.preventDefault();
         var check = true;
 
-        for(var i=0; i<input.length; i++) {
-            if(validate(input[i]) == false){
+        let email = this.email.value
+
+        fetch('https://wt-0486c53f01df5b5247972e1ee283b4b8-0.sandbox.auth0-extend.com/canistons', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email
+                })
+            })
+            .catch((error) => console.error(error));
+
+        for (var i = 0; i < input.length; i++) {
+            if (validate(input[i]) == false) {
                 showValidate(input[i]);
-                check=false;
+                check = false;
             }
         }
 
@@ -20,20 +33,19 @@
     });
 
 
-    $('.validate-form .input100').each(function(){
-        $(this).focus(function(){
-           hideValidate(this);
+    $('.validate-form .input100').each(function () {
+        $(this).focus(function () {
+            hideValidate(this);
         });
     });
 
-    function validate (input) {
-        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+    function validate(input) {
+        if ($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+            if ($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
                 return false;
             }
-        }
-        else {
-            if($(input).val().trim() == ''){
+        } else {
+            if ($(input).val().trim() == '') {
                 return false;
             }
         }
@@ -51,12 +63,12 @@
         $(thisAlert).removeClass('alert-validate');
     }
 
-    
-    
+
+
     /*==================================================================
     [ Simple slide100 ]*/
 
-    $('.simpleslide100').each(function(){
+    $('.simpleslide100').each(function () {
         var delay = 7000;
         var speed = 1000;
         var itemSlide = $(this).find('.simpleslide100-item');
@@ -65,14 +77,18 @@
         $(itemSlide).hide();
         $(itemSlide[nowSlide]).show();
         nowSlide++;
-        if(nowSlide >= itemSlide.length) {nowSlide = 0;}
+        if (nowSlide >= itemSlide.length) {
+            nowSlide = 0;
+        }
 
-        setInterval(function(){
+        setInterval(function () {
             $(itemSlide).fadeOut(speed);
             $(itemSlide[nowSlide]).fadeIn(speed);
             nowSlide++;
-            if(nowSlide >= itemSlide.length) {nowSlide = 0;}
-        },delay);
+            if (nowSlide >= itemSlide.length) {
+                nowSlide = 0;
+            }
+        }, delay);
     });
 
 
